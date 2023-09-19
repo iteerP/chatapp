c376591dc40e08845f0e091a19230483a80f7e53
Preeti Gobindpuri <preetigobindpuri02@gmail.com>
2023-09-19 00:29:40 -0700

    add environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCudiBu63ZHu5J2C2Las6PWtWGcyRFBOD4",
-  authDomain: "chat-67cdb.firebaseapp.com",
-  projectId: "chat-67cdb",
-  storageBucket: "chat-67cdb.appspot.com",
-  messagingSenderId: "163551992590",
-  appId: "1:163551992590:web:3bb7d005134a43f172155f",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
